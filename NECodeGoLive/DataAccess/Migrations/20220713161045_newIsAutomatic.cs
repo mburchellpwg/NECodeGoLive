98b931f77299cc09968f9424169d008a2f2ea7ea
mburchellpwg <mburchell@phoenixwebgroup.com>
2022-07-13 11:13:03 -0500

    migrationScript for IsAutomatic

---
+++ NECodeGoLive/DataAccess/Migrations/20220713161045_newIsAutomatic.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class newIsAutomatic : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAutomatic",
+                table: "Autos",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAutomatic",
+                table: "Autos");
+        }
+    }
+}
