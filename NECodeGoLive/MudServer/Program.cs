7fba8cdfb5efc41144b11ce43641fe5012747844
mburchellpwg <mburchell@phoenixwebgroup.com>
2022-07-13 14:16:47 -0500

    identity failure

--- NECodeGoLive/MudServer/Program.cs
+++ NECodeGoLive/MudServer/Program.cs
@@ -6,7 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 //builder.Services.AddDbContext<ApplicationDbContext>(options =>
 //options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
